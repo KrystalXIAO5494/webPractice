d72dc29896d89140383600f5adcd10f32e20e1a7
Wanyue Xiao <wxiao5@illinois.edu>
2021-03-25 16:58:16 -0500

    restful comments index

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,8 +1,36 @@
+const { text } = require('express');
 const express = require('express');
 const app= express();
+const path =require("path");
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
+app.set('view',path.join(__dirname,'view'))
+app.set('view engine','ejs')
+
+const comments =[
+    {
+        username:"todd",
+        comments:'lol so funny'
+    },
+    {
+        username:"amy",
+        comments:'lol '
+    },
+    {
+        username:"nancy",
+        comments:'funny'
+    },
+    {
+        username:"bob",
+        comments:'lol funny'
+    }
+]
+
+app.get('/comments',(req,res)=>{
+    res.render('comments/index',{comments})
+})
+
 
 app.get('/tacos',(req,res)=>{
     res.send("get/tacos response")
@@ -14,3 +42,4 @@ app.post('/tacos',(req,res)=>{
 app.listen(3000,()=>{
     console.log("on port 3000")
 })
+
